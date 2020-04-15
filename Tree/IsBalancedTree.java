fed968437b595489294181cfe2fada7d5fc4daa7
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2020-04-15 12:55:32 +0530

    Posted Tree/IsBalancedTree.java

---
+++ Tree/IsBalancedTree.java
@@ -0,0 +1,20 @@
+package Tree;
+
+public class IsBalancedTree {
+
+    boolean isBalanced(Node root)
+    {
+	    if (root == null) return true;
+	    
+	    if ((Math.abs(height(root.left) - height(root.right)) <=1) && isBalanced(root.left) && isBalanced(root.right))
+	        return true;
+	    return false;
+    }
+    
+    int height(Node root) {
+        if (root == null) return 0;
+        
+        return (1 + Math.max(height(root.left), height(root.right)));
+    }
+    
+}
