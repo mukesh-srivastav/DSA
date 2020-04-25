4377b422f9760d78d9e9b4cc1bf295196fd82ac7
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2020-04-25 16:45:22 +0530

    Remove unnecessary import statement

--- Heap/RearrangeCharacters.java
+++ Heap/RearrangeCharacters.java
@@ -1,7 +1,6 @@
 package Heap;
 
 import java.util.*;
-import java.io.*;
 
 /**
  * Given a string S with repeated characters (only lowercase). The task is to rearrange characters in a string such that no two adjacent characters are same.
