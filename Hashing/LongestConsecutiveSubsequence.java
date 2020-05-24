1ff5717b39fe3660b059de3acc124dc500ea8793
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2020-05-24 13:09:55 +0530

    Posted Hashing/LongestConsecutiveSubsequence.java

---
+++ Hashing/LongestConsecutiveSubsequence.java
@@ -0,0 +1,43 @@
+package Hashing;
+
+import java.util.*;
+
+class LongestConsecutiveSubsequence {
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while (t-- > 0) {
+		    int n = sc.nextInt();
+		    
+		    int a [] = new int[n];
+		    HashSet<Integer> hs = new HashSet<Integer>();
+		    
+		    for (int i=0; i<n; i++) {
+		        a[i] = sc.nextInt();
+		        hs.add(a[i]);
+		    }
+		    
+		    int max = 0;
+		    for (int i=0; i<n; i++) {
+		        // check If a[i] is not the part of subsequence already.
+		        if (hs.contains(a[i] - 1) == false) {
+    		        int j = a[i];
+    		        
+    		        int c = 0;
+    		        
+    		        while (hs.contains(j))
+    		            j++;
+    		        c = j - a[i];
+    		        
+    		        if (c>max)
+    		            max = c;
+		        }
+		    }
+		    
+		    System.out.println(max);
+        }
+        sc.close();
+	}
+}
